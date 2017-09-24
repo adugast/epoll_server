e5bda7b6568edf6f4a5fe7c4c239c8ae448d39fe
pestbuns <ant.one.dugast@gmail.com>
2017-09-24 15:59:11 +0200

    [epoll_server] add get_listening_socket

--- poll_server/src/main.c
+++ poll_server/src/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#include <unistd.h>
+
+#include "my_socket.h"
+
+int epoll_server(int ip_addr, int port)
+{
+    int ret = -1;
+    int sockfd = -1;
+
+    sockfd = get_listening_socket(ip_addr, port);
+    if (ret == -1) {
+        return -1;
+    }
+
+    ret = close(sockfd);
+    if (ret == -1) {
+        perror("close()");
+        return -1;
+    }
+
+    return 0;
+}
 
 int entry()
 {
     printf("[entry]\n");
+    epoll_server(0x7f000001, 9999);
     return 0;
 }
 
---
+++ poll_server/src/my_socket.h
@@ -0,0 +1,6 @@
+#ifndef __MY_SOCKET_H__
+#define __MY_SOCKET_H__
+
+int get_listening_socket(int ip_addr, int port);
+
+#endif /* __MY_SOCKET_H__ */
